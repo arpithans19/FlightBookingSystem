5ea24ce24dd0795d41bb433369226b8208c96a1a
arpithans19 <arpithans1101@gmail.com>
2022-07-25 16:36:16 +0530

    flightbooking system added.

---
+++ check-in/src/main/java/com/checkin/controller/HelloworldController.java
@@ -0,0 +1,18 @@
+package com.checkin.controller;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableEurekaClient
+public class HelloworldController {	
+	
+	@RequestMapping("/hellworld")
+	public String helloWorld() {
+		return "hello world -checkin ";
+	}	
+	
+	
+
+}
